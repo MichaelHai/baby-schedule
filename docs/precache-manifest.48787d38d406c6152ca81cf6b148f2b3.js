f6843506935ed8d2fbc546b5de09dc9d9a2873af
Wang Hai <wang_ha@worksap.co.jp>
2019-12-26 14:31:16 +0800

    add docs for github pages

---
+++ docs/precache-manifest.48787d38d406c6152ca81cf6b148f2b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f87c9fa7e5484960bd",
+    "url": "/css/chunk-vendors.640835c2.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9a035c1acf65ea9cc8fd702ad51187de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ecf698e323280d2eb4e",
+    "url": "/js/app.22e158b3.js"
+  },
+  {
+    "revision": "a8f87c9fa7e5484960bd",
+    "url": "/js/chunk-vendors.b210167a.js"
+  },
+  {
+    "revision": "fc637079fe469d3f6cabca7b2bb56b76",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
